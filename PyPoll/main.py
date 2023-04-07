f8bcbbe2fcda0555ec09de4dfb2ae58f6c0de597
K-Beal <beal.katie5@gmail.com>
2023-04-07 16:40:55 -0400

    calculated percentage, winner, exported text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,28 +4,47 @@ import csv
 csvpath = os.path.join('.', 'Resourcespoll', 'election_data.csv')
 
 #creating lists and defining variables
-number_of_votes = []
 list_of_candidates = []
 candidates = dict()
-
+total_votes = 0
 with open(csvpath, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-#the total number of votes cast    
+    #the total number of votes cast    
     for row in (csvreader):
-        number_of_votes.append(row[0])
-# total votes per candidate
+        total_votes += 1
+        # total votes per candidate
         candidate = row[2]
         if candidate in candidates:
             candidates[candidate] += 1
         else:
             candidates[candidate] = 1
-#percent of votes per candidate
-    votes = number_of_votes[candidate]
-    percent_vote = (float(votes)/float(number_of_votes) * 100)
 
-print(f"Total Votes: {len(number_of_votes)}")
-print(f"List of Candidates: {candidates}")
-print(f"vote percent: {percent_vote}")
+#Find the Winner
+winner = ""
+most_votes = max(candidates.values())
+for candidate in candidates:
+    if candidates[candidate] == most_votes:
+        winner = candidate
+print(f"Total Votes: {total_votes}")
+#Find percent of votes won per candidate
+for candidate in candidates:
+    percent_vote = 100 * candidates[candidate] / total_votes
+    # print(candidate, percent_vote)
+    print(f"Vote Percentage: ({candidate}, {percent_vote:0.2f}, {votes}")
+
+print(f"Winner: {winner}")
+
+with open(os.path.join("output-file.txt"), "w") as textfile:
+    textfile.write("Election Results\n")
+    textfile.write("---------------------\n")
+    textfile.write(f"Total Votes: {total_votes}\n")
+    textfile.write("---------------------\n")
+    for candidate in candidates:
+        percent_vote = 100 * candidates[candidate] / total_votes
+        textfile.write(f"Vote Percentage: ({candidate}, {percent_vote:0.2f}%, {votes}\n")
+    textfile.write("---------------------\n")
+    textfile.write(f"Winner: {winner}\n")
+    textfile.write("---------------------\n")
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # python-challenge
 
 Due Date: April 6, 2023
+asked for extension
 
 Worked with Dr. John Wagner on PyBank code April 4
 Finally got the output-file.txt correct April 4
+Worked with tutor and Dr. Wagner on 4/7
