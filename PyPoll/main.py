4b5d2c2fe7165a4bbd71520f83a1a2f3634654cf
K-Beal <beal.katie5@gmail.com>
2023-04-07 22:29:36 -0400

    added votes per candidate and corrected text file format

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,7 @@ import csv
 csvpath = os.path.join('.', 'Resourcespoll', 'election_data.csv')
 
 #creating lists and defining variables
-list_of_candidates = []
+
 candidates = dict()
 total_votes = 0
 with open(csvpath, "r") as csvfile:
@@ -33,8 +33,9 @@ print(f"Total Votes: {total_votes}")
 for candidate in candidates:
     percent_vote = 100 * candidates[candidate] / total_votes
     # print(candidate, percent_vote)
-    print(f"Vote Percentage: ({candidate}, {percent_vote:0.2f}, {votes}")
-
+    print(f"Vote Percentage: {candidate}, {percent_vote:0.3f}, ({candidates[candidate]})")
+# num_vote = total_votes/percent_vote
+# print(num_vote)
 print(f"Winner: {winner}")
 
 with open(os.path.join("output-file.txt"), "w") as textfile:
@@ -44,7 +45,7 @@ with open(os.path.join("output-file.txt"), "w") as textfile:
     textfile.write("---------------------\n")
     for candidate in candidates:
         percent_vote = 100 * candidates[candidate] / total_votes
-        textfile.write(f"Vote Percentage: ({candidate}, {percent_vote:0.2f}%, {votes}\n")
+        textfile.write(f"Vote Percentage: {candidate}, {percent_vote:0.3f}%, ({candidates[candidate]})\n")
     textfile.write("---------------------\n")
     textfile.write(f"Winner: {winner}\n")
     textfile.write("---------------------\n")
